8e4a3a2e640460ada599ef8a5c8f0c87957d6178
Zainul Setyo P <just_my_freak@yahoo.co.id>
2014-10-17 12:45:15 +0700

    add prepare data function to check whether number is even or not. and then push to array

--- tools/magicnumbers.js
+++ tools/magicnumbers.js
@@ -2,6 +2,7 @@ var _ = require("underscore");
 var fs = require("fs");
 
 var magic = {
+	validData: [],
 	isMagicNumber: function(value) {
 		var valInt = parseInt(value);
 		// console.log(valInt);
@@ -18,7 +19,26 @@ var magic = {
 			return true;
 
 		return false;
+	},
+	prepareData: function(totalData) {
+		// loop for total data
+		for (var i = 0; i <= totalData; i++) {
+			// count number
+			var numStr = i.toString();
+
+			// if under 10, add 0
+			if (i < 10) {
+				var str = "0"+i;
+				// push valid even number to array
+				this.validData.push(str);
+			} else if ((numStr.length % 2) == 0) {
+				var str = i.toString();
+				this.validData.push(str);
+			}
+		}
 	}
 }
 
+magic.prepareData(1000);
+console.log(magic.validData);
 console.log(magic.isMagicNumber('00'));
