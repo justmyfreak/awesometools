1aee0e36aa0ba674569d0e8cff1169fd4299993a
Zainul Setyo P <just_my_freak@yahoo.co.id>
2014-10-17 08:57:06 +0700

    add check function to do loop on given number

--- tools/primenumber.js
+++ tools/primenumber.js
@@ -1,20 +1,37 @@
-var number = 20;
-var delimeter = ", ";
+var count = 20;
 var and = "and";
 
 var res = "";
 var prime = {
+	primes: [],
+	result: "",
+	delimeter: ", ",
 	isPrime: function (number) {
 		if (number < 2)
 			return false;
 
 		for (var i = 2; i < number; i++) {
-			// console.log(i);
 			if (number % i == 0) 
 				return false;
 		}
 		return true;
+	},
+	check: function(count) {
+		for (var i = 2; i <= count; i++) {
+			if (this.isPrime(i)) 
+				this.primes.push(i);
+		}
+	},
+	render: function() {
+		if (this.primes.length == 1) {
+			this.result += this.primes[0];
+			break;
+		}
+
+		
 	}
 };
 
-console.log(prime.isPrime(2));
+prime.check(2);
+prime.render()
+console.log(prime.result);
